9169238959166639237d7f89afcfadbf21744084
Marcos <marcosmartinezpalermo@gmail.com>
2022-02-17 22:43:02 -0300

    Dev exception page

--- hp-api/Startup.cs
+++ hp-api/Startup.cs
@@ -92,10 +92,7 @@ namespace hp_api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
 
